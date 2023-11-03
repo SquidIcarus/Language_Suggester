19d678671c2740f56770bf4c1b536829be9d6049
Landon Isaiah <landonisaiah@gmail.com>
2023-11-03 13:36:43 -0700

    Add else if branch to radio selector question.

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,16 @@ window.onload = function () {
 
         let languageResult = document.getElementById("whatlanguage");
         languageResult.classList.add("hidden");
+        let languageResult2 = document.getElementById("otherlanguage");
+        languageResult2.classList.add("hidden");
 
         const numbChoice = document.querySelector('input[name="numb"]:checked');
         const question2 = document.querySelector("#question2").value;
 
         if (numbChoice && numbChoice.value === "13") {
             languageResult.classList.remove("hidden");
+        } else if (numbChoice && numbChoice.value === "22") {
+            languageResult2.classList.remove("hidden");
         }
 
         if (question2.toLowerCase() === "red") {
