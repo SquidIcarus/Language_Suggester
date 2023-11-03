3d807cd8fba26b1d1617de00ea17493ef81909fb
Landon Isaiah <landonisaiah@gmail.com>
2023-11-03 12:31:21 -0700

    Add two question inputs with a result.

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,21 @@ window.onload = function () {
     form.onsubmit = function (event) {
         event.preventDefault();
 
-    }
+
+        let languageResult = document.getElementById("whatlanguage");
+        languageResult.classList.add("hidden");
+        let languageResult = document.getElementById("whatlanguage");
+        languageResult.classList.add("hidden");
+
+        const question1 = parseInt(document.querySelector("#question1").value);
+        const question2 = document.querySelector("#question2").value;
+
+        if (question1 > 21) {
+            languageResult.classList.remove("hidden");
+        }
+
+        if (question2.toLowerCase() === "red") {
+            languageResult.classList.remove("hidden");
+        }
+    };
 };
