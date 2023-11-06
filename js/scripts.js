d672cc1d88239a7becc6f22e8a6348c0f7ec5010
Landon Isaiah <landonisaiah@gmail.com>
2023-11-05 23:22:28 -0800

    Swap question 1 and 2 & change input style.

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@ window.onload = function () {
     form.onsubmit = function (event) {
         event.preventDefault();
 
-        const numbChoice = document.querySelector('input[name="numb"]:checked');
-        const question2 = document.querySelector("#question2").value;
+        const numbChoice = document.querySelector("#numInput").value;
+        const question2 = document.querySelector('input[name="question2"]:checked').value;
         const question3 = document.querySelector("#question3").value;
         const question4 = document.querySelector("#question4").value;
         const question5 = document.querySelector("#question5").value;
 
         let response = "";
 
-        if (numbChoice && parseInt(numbChoice.value) <= 13 && question4.toLowerCase() !== "black") {
+        if (parseInt(numbChoice) <= 13 && question4.toLowerCase() !== "black") {
             response += "You like spooky numbers! ";
-        } else if (numbChoice && parseInt(numbChoice.value) >= 22 && parseInt(numbChoice.value) < 99 && question4.toLowerCase() !== "black") {
+        } else if (parseInt(numbChoice) >= 22 && parseInt(numbChoice) <= 99 && question4.toLowerCase() !== "black") {
             response += "Keeping it simple i see ";
-        } else if (numbChoice && parseInt(numbChoice.value) >= 100 && question4.toLowerCase() !== "black") {
+        } else if (parseInt(numbChoice) >= 100 && question4.toLowerCase() !== "black") {
             response += "Oh you like numbers? ";
         }
 
@@ -43,11 +43,11 @@ window.onload = function () {
             response += "Maybe seek out another hobbie..."
         }
 
-        if (question5.toLowerCase() === "I eat sleep and breath it!" && question4.toLowerCase() !== "black") {
+        if (question5.toLowerCase() === "i eat sleep and breath it!" && question4.toLowerCase() !== "black") {
             response += "C#";
-        } else if (question5.toLowerCase() === "Have you seen my MySpace page?" && question4.toLowerCase() !== "black") {
+        } else if (question5.toLowerCase() === "have you seen my myspace page?" && question4.toLowerCase() !== "black") {
             response += "HTML";
-        } else if (question5.toLowerCase() === "I'm a sodoku master!" && question4.toLowerCase() !== "black") {
+        } else if (question5.toLowerCase() === "i'm a sodoku master!" && question4.toLowerCase() !== "black") {
             response += "CSS";
         } else if (question5.toLowerCase() === "code?" && question4.toLowerCase() !== "black") {
             response += "01001001 01110100 00100111 01110011 00100000 01101111 01101011 01100001 01111001 00101100 00100000 01110100 01110010 01111001 00100000 01101111 01110101 01110100 00100000 01000101 01110000 01101001 01100011 01101111 01100100 01110101 01110011 00101100 00100000 01110100 01101000 01100101 01111001 00100000 01110111 01101001 01101100 01101100 00100000 01110011 01101000 01101111 01110111 00100000 01111001 01101111 01110101 00100000 01110100 01101000 01100101 00100000 01110111 01100001 01111001 00100001";
